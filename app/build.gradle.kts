53cafe43015ebefbaa80cfb8125a48aff8dc8055
B67687 <111849193+B67687@users.noreply.github.com>
2026-05-09 10:36:44 +0800

    Bump version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bushop.sg"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.6.0"
+        versionCode = 6
+        versionName = "0.6.1"
 
 
         vectorDrawables {
